7ea110ec6c572bf011672dfd33c8ed1fdba8f0bb
ra8200 <ra8200@hotmail.com>
2021-08-20 01:26:41 -0400

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,38 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" exact>
-            Razi
+    <header className="bg-blue-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-grey-100"
+            className="inline-flex items-center py-6 px-3 mr-4 text-white hover:text-grey-800 text-4xl font-bold styleMe tracking-widest"
+          >
+            Razi Alawamleh
           </NavLink>
-          <NavLink to="/post">Blog Post</NavLink>
-          <NavLink to="/project">Projects</NavLink>
-          <NavLink to="/about">About Me!</NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 my-6 rounded text-blue-100 hover:text-grey-800"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Blog Post
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 my-6 rounded text-blue-200 hover:text-grey-800"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 my-6 rounded text-blue-200 hover:text-grey-800"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            About Me!
+          </NavLink>
         </nav>
       </div>
     </header>
