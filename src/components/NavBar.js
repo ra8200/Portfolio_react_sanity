b28dfec126992b2628ec599b0d74c967965d37b3
ra8200 <ra8200@hotmail.com>
2021-08-20 00:51:00 -0400

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            Razi
+          </NavLink>
+          <NavLink to="/post">Blog Post</NavLink>
+          <NavLink to="/project">Projects</NavLink>
+          <NavLink to="/about">About Me!</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/SinglePost.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SingPost() {
+  return <h1>SinglePost Page!</h1>;
+}
