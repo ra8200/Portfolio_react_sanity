590fd53e72ca3543cfbe117540e365a84642616d
ra8200 <ra8200@hotmail.com>
2021-08-20 17:28:53 -0400

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
@@ -9,33 +10,63 @@ export default function NavBar() {
           <NavLink
             to="/"
             exact
-            activeClassName="text-grey-100"
-            className="inline-flex items-center py-6 px-3 mr-4 text-white hover:text-grey-800 text-4xl font-bold styleMe tracking-widest"
+            activeClassName="text-blue-800"
+            className="inline-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-4xl font-bold styleMe tracking-widest"
           >
             Razi Alawamleh
           </NavLink>
           <NavLink
             to="/post"
-            className="inline-flex items-center py-3 my-6 rounded text-blue-100 hover:text-grey-800"
-            activeClassName="text-blue-100 bg-blue-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-black"
+            activeClassName="bg-blue-800"
           >
             Blog Post
           </NavLink>
           <NavLink
             to="/project"
-            className="inline-flex items-center py-3 my-6 rounded text-blue-200 hover:text-grey-800"
-            activeClassName="text-blue-100 bg-blue-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-black"
+            activeClassName="bg-blue-800"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
-            className="inline-flex items-center py-3 my-6 rounded text-blue-200 hover:text-grey-800"
-            activeClassName="text-blue-100 bg-blue-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-black"
+            activeClassName="bg-blue-800"
           >
             About Me!
           </NavLink>
         </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://github.com/ra8200"
+            className="mr-4"
+            target="_blank"
+            fgcolor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/razi-alawamleh/"
+            className="mr-4"
+            target="_blank"
+            fgcolor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.instagram.com/blinksart/"
+            className="mr-4"
+            target="_blank"
+            fgcolor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://twitter.com/Blink_Arts"
+            className="mr-4"
+            target="_blank"
+            fgcolor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
       </div>
     </header>
   );
