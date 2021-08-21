6a5a5fee49e0bb9977c6630e4a901e8e7b44c09d
ra8200 <ra8200@hotmail.com>
2021-08-20 20:45:38 -0400

    blog post page built out and connected to sanity

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,59 @@
 import React, { useState, useEffect } from "react";
-import sanityClient from "../client"
+import { Link } from "react-router-dom";
+import sanityClient from "../client";
 
 export default function Post() {
-  const [postData, setPost] + useState(null);
+  const [postData, setPost] = useState(null);
 
-  useEffect() => {
+  useEffect(() => {
     sanityClient
-      .fetch(`*[_type == "post"] {
+      .fetch(
+        `*[_type == "post"] {
         title,
         slug,
         mainImage{
-          _id.
+          _id,
           url
         },
         alt
-      }`)
-  }
-  
-  return 
+      }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.err);
+  }, []);
+
+  return (
+    <main className="bg-blue-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center styleMe">Blog Post Page</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          Welcome to my Blog Page
+        </h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span
+                    className="block h-65 relative rounded shadow leading-snug bg-white border-l-8 border-blue-400"
+                    key={index}
+                  >
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="w-full h-full rounded-r object-cover absolute"
+                    />
+                    <span className="blog relative h-full flex justify-end items-end pr-4 pb-4">
+                      <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-blue-700 text-white bg-opacity-75 rounded">
+                        {post.title}
+                      </h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  );
+}
