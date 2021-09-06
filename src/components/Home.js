13add8132c06ac96e8ff0b2041fbf483ffe5f800
ra8200 <ra8200@hotmail.com>
2021-09-06 14:06:16 -0400

    fixed home message

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
       />
       <section className="relative flex justify-center min-h-screen pt-12 lg:64 px-8">
         <h1 className="text-6xl text-blue-500 font-bold styleMe leading-none lg:leading-snug home-name">
-          Hello, I'm Razi. Welcome to my portfolio.
+          Hello, I'm Razi.
         </h1>
       </section>
     </main>
