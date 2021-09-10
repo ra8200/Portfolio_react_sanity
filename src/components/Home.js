dea73443873ea925ab15af7fcdb7e5b0c29d09a3
ra8200 <ra8200@hotmail.com>
2021-09-10 14:39:24 -0400

    Hello Razi

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
         className="absolute object-cover w-full h-full"
       />
       <section className="relative flex justify-center min-h-screen pt-12 lg:64 px-8">
-        <h1 className="text-6xl text-blue-500 font-bold styleMe leading-none lg:leading-snug home-name">
+        <h1 className=" relative justify-center text-6xl text-blue-500 font-bold styleMe leading-none lg:leading-snug home-name">
           Hello, I'm Razi.
         </h1>
       </section>
